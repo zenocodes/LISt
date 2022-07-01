b7d41ec88e33d45ede1c7179eedca392efd08791
zenocodes <azronebrian@gmail.com>
2022-07-01 11:10:33 +0300

    connected to the db

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import express from 'express'
+import mysql from 'mysql'
 
 const app = express()
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'list_app_v2'
+})
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
 
 /* ROUTES */
 
@@ -25,6 +33,18 @@ app.get('/list/:id', (req, res) => {
 // CREATE A LIST
 app.get('/create', (req, res) => {
     res.render('create-list')
+})
+
+app.post('/create', (req, res) => {
+    let sql = 'INSERT INTO list (title, body) VALUES (?,?)'
+
+    connection.query(
+        sql,
+        [req.body.title, req.body.body], 
+        (error, results) => {
+            res.redirect('/lists')
+        }
+    )
 })
 
 // EDIT A LIST
